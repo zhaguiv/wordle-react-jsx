b0e9f1acb3d2456997dd4274789751ce2f425c0d
zhaguiv <zhaguiv@gmail.com>
2024-07-01 11:18:48 -0500

    Checking in comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Board from './components/Board';
 
-
+//-App
+//--Board
+//---Row (6)
+//----Tile (5)
 function App() {
 
   const WORD_API_URL = `https://random-word-api.herokuapp.com/word?length=5`;
